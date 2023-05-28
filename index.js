26cc3eb59655c21e6cab9fcad2ea93c246a1b926
xKollawaTx <eark050346@gmail.com>
2023-05-29 02:12:45 +0700

    collection create

--- index.js
+++ index.js
@@ -95,28 +95,30 @@ app.post("/signin", (req, res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send({ message: "Internal Server Error" })
-    })
-})
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
 
-//addgame section
-const gameSchema = mongoose.Schema({
-  image: String,
-  name: String,
-  platform: String,
-  gener: String,
-  rating: String,
-  publisher: String,
-  dateAdded: { type: Date, default: Date.now }
-}, { timestamps: true });
+//game section
+const gameSchema = mongoose.Schema(
+  {
+    image: String,
+    name: String,
+    platform: String,
+    gener: String,
+    rating: String,
+    publisher: String,
+    dateAdded: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 const gameModel = mongoose.model("game", gameSchema);
 
-//save game data
-//api
+//add game by admin
 app.post("/addgame", async (req, res) => {
-  console.log(req.body)
-  const { name } = req.body
-  const { platform } = req.body
+  console.log(req.body);
+  const { name } = req.body;
+  const { platform } = req.body;
   gameModel
     .findOne({ name: name }, { platform: platform })
     .exec()
@@ -124,20 +126,20 @@ app.post("/addgame", async (req, res) => {
       if (result) {
         if (result.platform === platform) {
           if (result.name === name) {
-            res.send({ message: "Game Already Exist", alert: "false" })
+            res.send({ message: "Game Already Exist", alert: "false" });
           }
         }
       } else {
-        const data = gameModel(req.body)
-        const datasave = data.save()
-        res.send({ message: "Game added successfully", alert: "true" })
+        const data = gameModel(req.body);
+        const datasave = data.save();
+        res.send({ message: "Game added successfully", alert: "true" });
       }
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send({ message: "Internal Server Error" })
-    })
-})
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
 
 // get game data
 app.get("/game", (req, res) => {
@@ -145,13 +147,13 @@ app.get("/game", (req, res) => {
     .find({})
     .exec()
     .then((data) => {
-      res.send((JSON.stringify(data)));
+      res.send(JSON.stringify(data));
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send({ message: "Internal Server Error" })
-    })
-})
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
 
 // Search game by name
 app.get("/searchgame", (req, res) => {
@@ -166,13 +168,16 @@ app.get("/searchgame", (req, res) => {
       if (data.length > 0) {
         res.status(200).json(data);
       } else {
-        gameModel.findOne({ name }).exec().then((game) => {
-          if (game) {
-            res.status(200).json([game]);
-          } else {
-            res.status(404).json({ message: "Game not found" });
-          }
-        });
+        gameModel
+          .findOne({ name })
+          .exec()
+          .then((game) => {
+            if (game) {
+              res.status(200).json([game]);
+            } else {
+              res.status(404).json({ message: "Game not found" });
+            }
+          });
       }
     })
     .catch((err) => {
@@ -228,7 +233,7 @@ app.get("/game/:id", (req, res) => {
     .exec()
     .then((game) => {
       if (game) {
-        res.send(JSON.stringify(game))
+        res.send(JSON.stringify(game));
       } else {
         res.status(404).json({ message: "Game not found" });
       }
@@ -245,13 +250,128 @@ app.get("/user", (req, res) => {
     .find({})
     .exec()
     .then((data) => {
-      res.send((JSON.stringify(data)));
+      res.send(JSON.stringify(data));
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).send({ message: "Internal Server Error" })
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
+
+// Update user account
+app.post("/updateAccount", (req, res) => {
+  const { email, username, password, confirmPassword, image } = req.body;
+
+  // Update the user in the database
+  userModel
+    .findOneAndUpdate(
+      { email: email },
+      {
+        username: username,
+        password: password,
+        confirmPassword: confirmPassword,
+        image: image,
+      },
+      { new: true }
+    )
+    .exec()
+    .then((updatedUser) => {
+      res.send({ message: "Account updated successfully", user: updatedUser });
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
+
+//collection section
+const collectionSchema = mongoose.Schema({
+  name: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  game: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "game",
+  },
+});
+const collectionModel = mongoose.model("collection", collectionSchema);
+
+//Create collection
+app.post("/createcollection", (req, res) => {
+  const { name, userId, gameId } = req.body;
+  collectionModel
+    .findOne({ name: name, user: userId, game: gameId })
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.send({ message: "Collection Already Exist", alert: "false" });
+      } else {
+        // Create a new collection
+        const newCollection = collectionModel({
+          name: name,
+          user: userId,
+          gameId: gameId,
+        });
+        // Save the collection to the database
+        newCollection
+          .save()
+          .then((savedCollection) => {
+            res.send({
+              message: "Collection created successfully",
+              alert: "true",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send({ message: "Internal Server Error" });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
+
+// Get collection data
+app.get("/collection", (req, res) => {
+  collectionModel
+    .find({})
+    .populate("user")
+    .populate("game")
+    .exec()
+    .then((data) => {
+      res.send(JSON.stringify(data));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ message: "Internal Server Error" });
+    });
+});
+
+// Get collection by user ID
+app.get("/collection/:id", (req, res) => {
+  const { id } = req.params;
+  collectionModel
+    .find({ user: id })
+    .populate("user")
+    .populate("game")
+    .exec()
+    .then((collection) => {
+      if (collection) {
+        res.send(JSON.stringify(collection));
+      } else {
+        res.status(404).json({ message: "Collection not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
+
 
 //server running
 app.listen(PORT, () => console.log("Server is running on port :" + PORT));
